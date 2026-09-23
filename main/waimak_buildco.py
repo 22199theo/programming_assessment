32b1e87e07a46a5f0388e48ead7f14648a6ad454
theovodde <theovodde@gmail.com>
2026-09-24 10:53:05 +1200

    customer function

--- main/waimak_buildco.py
+++ main/waimak_buildco.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
 
-
 class Customer:
     def __init__(self, name, address, delivery_address, customer_type):
         self.name = name
@@ -14,6 +13,18 @@ class Customer:
             return 0.1
         else:
             return 0
+
+
+
+def customer():
+    name = name_entry.get()
+    address = address_entry.get()
+    delivery_address = delivery_address_entry.get()
+    customer_type = customer_type_entry.get()
+
+    customer = Customer(name, address, delivery_address, customer_type)
+
+
 
 
 def main_screen():
