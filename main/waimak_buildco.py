f2069fc8850b38f3cc6e8e5c2319cbddc7e1ed31
theovodde <theovodde@gmail.com>
2026-09-23 15:53:12 +1200

    Customer class

--- main/waimak_buildco.py
+++ main/waimak_buildco.py
@@ -1,1 +1,9 @@
-print ("Testing")
+class Customer:
+    def __init__(self, name, address, delivery_address, customer_type):
+        self.name = name
+        self.phone = address
+        self.delivery_address = delivery_address
+        self.customer_type = customer_type
+
+    
+    
