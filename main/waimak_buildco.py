83d8d29ba0ebb4c18977f68b1aeb3a8ffe5bf960
theovodde <theovodde@gmail.com>
2026-09-23 16:03:39 +1200

    discount

--- main/waimak_buildco.py
+++ main/waimak_buildco.py
@@ -5,5 +5,11 @@ class Customer:
         self.delivery_address = delivery_address
         self.customer_type = customer_type
 
-    
+    def customer_discount(self):
+        if self.customer_type == "trade_customer":
+            return 0.1
+        else:
+            return 0
+
+
     
