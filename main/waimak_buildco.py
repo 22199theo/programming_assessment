dfbd7d7768fec5da82b6a1a732694643c74b654a
theovodde <theovodde@gmail.com>
2026-09-25 12:41:41 +1200

    added if statements for each checkbutton

--- main/waimak_buildco.py
+++ main/waimak_buildco.py
@@ -107,10 +107,39 @@ def gui():
 
         customer = Customer(name, address, delivery_address, customer_type)
 
-        house = 
+        house = House()
 
         for x in customers:
             print (x.name, x.address)
+
+        if checkbuttons["TS"].get():
+            house.bathroom.append("TS")
+
+        if checkbuttons["UW"].get():
+            house.bathroom.append("UW")
+
+        if checkbuttons["IH"].get():
+            house.bathroom.append("IH")
+
+        if checkbuttons["DA"].get():
+            house.bathroom.append("DA")
+
+        if checkbuttons["MA"].get():
+            house.bathroom.append("MA")
+
+        if checkbuttons["SD"].get():
+            house.bathroom.append("SD")
+
+        if checkbuttons["LH"].get():
+            house.bathroom.append("LH")
+
+        if checkbuttons["BH"].get():
+            house.bathroom.append("BH")
+
+        print (house.bathroom)
+        print (house.kitchen)
+        print (house.living_room)
+        print (house.bedroom)
 
         messagebox.showinfo("Success", "Customer added")
 
